2d2acc8f47cba5b2f559ce4f99218b1fef82bce5
Eldon-498 <eldondaniel2@gmail.com>
2023-08-11 21:18:22 +0300

    SIgnIn component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
