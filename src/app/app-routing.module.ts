7db83c76b0c9fed00619da3b5746444e3e08cd2a
Eldon-498 <eldondaniel2@gmail.com>
2023-08-12 01:04:46 +0300

    toggle widhdrwa and deposit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LoginPageComponent } from './login-page/login-page.component'; 
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'sign-in', component: SignInComponent },
   { path: 'login', component: LoginPageComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent }
+
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  customerName: string = '';
+  customerId: string = '';
+  accountBalance: number = 0;
+  withdrawAmount: number = 0;
+  depositAmount: number = 0;
+  showWithdrawForm: boolean = false;
+  showDepositForm: boolean = false;
+
+  toggleWithdrawForm() {
+    this.showWithdrawForm = !this.showWithdrawForm;
+    this.showDepositForm = false; // Hide deposit form
+  }
+  
+  toggleDepositForm() {
+    this.showDepositForm = !this.showDepositForm;
+    this.showWithdrawForm = false; // Hide withdraw form
+  }
 
 }
