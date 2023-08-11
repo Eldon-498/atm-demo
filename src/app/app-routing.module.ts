0b57be89ceba7ce78bac05ecfbc9810f9db54fb9
Eldon-498 <eldondaniel2@gmail.com>
2023-08-11 22:43:36 +0300

    sign in component styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginPageComponent } from './login-page/login-page.component'; // Adjust the path as needed
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'login', component: LoginPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  customerId: string = '';
+  pin: string = '';
 
+  submitForm(): void {
+    if (this.customerId && this.pin) {
+      console.log('Sign-in form submitted');
+      console.log('Customer ID:', this.customerId);
+      console.log('PIN:', this.pin);
+    } else {
+      console.log('Please fill in all fields');
+    }
+  }
 }
